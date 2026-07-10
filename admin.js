0057df8f1cc7e35e26626a7cfe89b458c126aa7f
youssefmohamedd492008-web <youssefmohamedd492008@gmail.com>
2026-07-10 06:09:05 +0300

    Add files via upload

--- admin.js
+++ admin.js
@@ -26,4 +26,75 @@ document.getElementById("message").innerHTML =
 
 }
 
+}const revenue = document.getElementById('revenueChart');
+
+if(revenue){
+
+new Chart(revenue,{
+
+type:'line',
+
+data:{
+
+labels:[
+"Jan",
+"Feb",
+"Mar",
+"Apr",
+"May"
+],
+
+datasets:[{
+
+label:"Revenue",
+
+data:[
+12000,
+19000,
+15000,
+30000,
+45000
+]
+
+}]
+
+}
+
+});
+
+}
+
+
+
+const booking = document.getElementById('bookingChart');
+
+
+if(booking){
+
+new Chart(booking,{
+
+type:'doughnut',
+
+data:{
+
+labels:[
+"Confirmed",
+"Pending",
+"Cancelled"
+],
+
+datasets:[{
+
+data:[
+70,
+20,
+10
+]
+
+}]
+
+}
+
+});
+
 }
